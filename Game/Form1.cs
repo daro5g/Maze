b8e2c6b644154517e263be9ae682cf8e7393c657
daro5g <darowskib@gmail.com>
2016-01-24 13:31:28 +0100

    maze 1.0

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,18 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        private Point StartPoint;
+        private bool isGame = true;
+        int nrmapy = 1;
+        int cos = 15;
         public Form1()
         {
             InitializeComponent();
+            la.Visible = false;
+            game_board1.Image = Game.Properties.Resources.lvl1;
+            StartPoint = game_board1.Location;
+            StartPoint.Offset(200, 23);
+            StartMouse();
         }
 
         private void finish_MouseEnter(object sender, EventArgs e)
@@ -25,14 +35,81 @@ namespace Game
 
         private void StartMouse()
         {
-            Point start = game_board.Location;
-            start.Offset(10, 10);
-            Cursor.Position = PointToScreen(start);
+            Cursor.Position = PointToScreen(StartPoint);
         }
+
 
         private void label2_MouseEnter(object sender, EventArgs e)
         {
             StartMouse();
+        }
+
+        private void game_board1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(cos-- <= 0)
+            {
+                cos = 15;
+                GC.Collect();
+            }else
+            {
+                return;
+            }
+            //MessageBox.Show(e.X +" "+ e.Y);
+            //return;
+            if (isGame)
+            {
+                try
+                {
+                    var pixelcolor = ((Bitmap)game_board1.Image).GetPixel(e.X, e.Y);
+                    //MessageBox.Show(pixelcolor.ToString());
+                    //return;
+                    if (pixelcolor.R <= 30 && pixelcolor.G <= 30 && pixelcolor.B <= 30 && pixelcolor.A >= 230)
+                        StartMouse();
+                    if (pixelcolor.R >= 220 && pixelcolor.G <= 50 && pixelcolor.B <= 50 && pixelcolor.A >= 200)
+                        Wygrana();
+                }
+                catch { }
+            }
+        }
+
+        private void Wygrana()
+        {
+            nrmapy++;
+            switch (nrmapy)
+            {
+                case 2:
+                    game_board1.Image = Game.Properties.Resources.lvl2;
+                    StartPoint = game_board1.Location;
+                    StartPoint.Offset(472, 462);
+                    StartMouse();
+                    break;
+                case 3:
+                    game_board1.Image = Game.Properties.Resources.lvl3;
+                    la.Visible = true;
+                    StartPoint = game_board1.Location;
+                    StartPoint.Offset(254, 468);
+                    StartMouse();
+                    break;
+            }
+        }
+
+        private void LastWin()
+        {
+            isGame = false;
+            game_board1.SizeMode = PictureBoxSizeMode.StretchImage;
+            game_board1.Image = Game.Properties.Resources.scary_maze_game_tribute;
+            SoundPlayer sp = new SoundPlayer(Game.Properties.Resources.sound);
+            sp.Play();
+
+        }
+
+        private void la_MouseEnter(object sender, EventArgs e)
+        {
+            if (isGame)
+            {
+                la.Visible = false;
+                LastWin();
+            }
         }
     }
 }
